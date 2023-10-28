42b9b4fcb6d72c2ed093a2334de7d4935fd70fd5
zhichengliangnhl <zhi.cheng.liang@student.nhlstenden.com>
2023-10-28 12:24:06 +0200

    please help with js

--- catalog.php
+++ catalog.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="catalog-style.css" type="text/css">
-    <title>Catalog</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/catalog-j.js"></script>
 </head>
 
     <body>
@@ -16,7 +17,8 @@
         <!-- end of header starting with main now -->
         <main>
             <div class="home-title">
-                <p>Home &nbsp > &nbsp Shop</p>
+                <p><a href="main.html"> Home &nbsp > </a></p> 
+                <p> &nbsp Shop</p>
             </div>
             <!-- start of whole code, below is the grid container for everything -->
             <div class="grid-container">
@@ -38,12 +40,20 @@
                             <div class="line-style"><hr></div>
                             <h3 class="h3-style">Colors</h3>
                                 <div class="filters">
-                                    <div class="colors-spacing">
+                                    <!-- <div class="colors-spacing">
                                         <span class="circle green-circle"><a href=""></a></span>
                                         <span class="circle red-circle"><a href=""></a></span>
                                         <span class="circle yellow-circle"><a href=""></a></span>
                                         <span class="circle blue-circle"><a href=""></a></span>
                                         <span class="circle pink-circle"><a href=""></a></span>
+                                    </div> -->
+
+                                    <div class="color-spacing">
+                                        <span class="circle green-circle" onclick="changeImage('green')"></span>
+                                        <span class="circle red-circle" onclick="changeImage('red')"></span>
+                                        <span class="circle yellow-circle" onclick="changeImage('yellow')"></span>
+                                        <span class="circle blue-circle" onclick="changeImage('blue')"></span>
+                                        <span class="circle pink-circle" onclick="changeImage('pink')"></span>
                                     </div>
                                 </div>
                             <!-- some line inside the thing -->
@@ -68,28 +78,29 @@
                             <!-- some line inside the thing -->
                             <div class="line-style"><hr></div>
                             <div class="filters"><input type="submit" value="Apply Filter"></div>
+                            <!-- include -->
                     </div>
                 </div>
                 <!-- end of filet and start of the second section -->
                     <div class="h2-astyle"><h2>Arrivals</h2></div>
-                    <div class="h4-astyle"><h4>Showing 1-10 of 100 Products &nbsp Sort by: Most Popular</h4></div>
+                    <div class="h4-astyle"><h4>Showing 1-9 Products</h4></div>
                         <!-- column 2 starts here -->
                         <div class="column-2">
                             <div class="flex-items">
                                 <div class="bs-box">
-                                    <img src="./img/Sunny_socks_blue.jpg" alt="Sunny_socks_blue">
+                                    <img src="./img/Sunny_socks_blue.jpg" alt="Sunny_socks_blue" id="img-container-9">
                                     <p>Sunny's Blue Striped Sock</p>
                                 </div>
                             </div>
                             <div class="flex-items">
                                 <div class="gs-box">
-                                    <img src="./img/Sunny_socks_green.jpg" alt="Sunny_socks_green">
+                                    <img src="./img/Sunny_socks_green.jpg" alt="Sunny_socks_green" id="img-container-6">
                                     <p>Sunny's Green Striped Sock</p>
                                 </div>
                             </div>
                             <div class="flex-items">
                                 <div class="ps-box">
-                                    <img src="./img/Sunny_socks_pink_01.jpg" alt="Sunny_socks_pink_01">
+                                    <img src="./img/Sunny_socks_pink_01.jpg" alt="Sunny_socks_pink_01" id="img-container-10">
                                     <p>Sunny's Pink Striped Sock</p>
                                 </div>
                             </div>
@@ -98,7 +109,7 @@
                         <div class="column-3">
                             <div class="flex-items">
                                 <div class="rs-box">
-                                    <img src="./img/Sunny_socks_red.jpg" alt="Sunny_socks_uni_red">
+                                    <img src="./img/Sunny_socks_red.jpg" alt="Sunny_socks_uni_red" id="">
                                     <p>Sunny's Red Striped Sock</p>
                                 </div>
                             </div>
@@ -135,11 +146,182 @@
                                     <p>Sunny's Yellow Striped Sock</p>
                                 </div>
                             </div>
+                        </div>
+                        <div>
+                            
                         </div>     
                 <!-- end of the second section "arrivals" -->
             </div>
             <!-- end of the main grid class for everything -->
         </main>
+        <!-- javascript for color changing. -->
+        <script>
+function changeImage(phfil) {
+            var imageElement1 = document.getElementById('img-container-1');
+            var imageElement2 = document.getElementById('img-container-2');
+            var imageElement3 = document.getElementById('img-container-3');
+            var imageElement4 = document.getElementById('img-container-4');
+            var imageElement5 = document.getElementById('img-container-5');
+            var imageElement6 = document.getElementById('img-container-6');
+            var imageElement7 = document.getElementById('img-container-7');
+            var imageElement8 = document.getElementById('img-container-8');
+            var imageElement9 = document.getElementById('img-container-9');
+            var imageElement10 = document.getElementById('img-container-10');
+
+            function changeImage(className) {
+    var imageElement1 = document.getElementById('img-container-1');
+    var imageElement2 = document.getElementById('img-container-2');
+    var imageElement3 = document.getElementById('img-container-3');
+    var imageElement4 = document.getElementById('img-container-4');
+    var imageElement5 = document.getElementById('img-container-5');
+    var imageElement6 = document.getElementById('img-container-6');
+    var imageElement7 = document.getElementById('img-container-7');
+    var imageElement8 = document.getElementById('img-container-8');
+    var imageElement9 = document.getElementById('img-container-9');
+    var imageElement10 = document.getElementById('img-container-10');
+
+    var imagePath1;
+    var imagePath2;
+    var imagePath3;
+    var imagePath4;
+    var imagePath5;
+    var imagePath6;
+    var imagePath7;
+    var imagePath8;
+    var imagePath9;
+    var imagePath10;
+
+    // Set the image paths based on the selected paragraph
+    switch (className) {
+        case 'phfil unicolor':
+            imagePath1 = './img/Sunny_socks_uni_green.jpg';
+            imagePath2 = './img/Sunny_socks_uni_red.jpg';
+            imagePath3 = './img/Sunny_socks_uni_yellow.jpg';
+            imagePath4 = './img/Sunny_socks_uni_blue.jpg';
+            imagePath5 = './img/Sunny_socks_uni_pink.jpg';
+            break;
+        case 'phfil strip-s':
+            imagePath1 = './img/Sunny_socks_green.jpg';
+            imagePath2 = './img/Sunny_socks_red.jpg';
+            imagePath3 = './img/Sunny_socks_yellow.jpg';
+            imagePath4 = './img/Sunny_socks_blue.jpg';
+            imagePath5 = './img/Sunny_socks_pink_01.jpg';
+            break;
+        case 'phfil n-arrivals':
+            imagePath1 = '../Webs/img/Sunny_socks_green.jpg';
+            imagePath2 = '../Webs/img/packaging/catalogus_sokken_stripes_green.jpg';
+            break;
+        case 'phfil outstanding-p':
+            // Set the outstanding-products image path
+            break;
+        default:
+            imagePath1 = './img/Sunny_socks_uni_green.jpg';
+            imagePath2 = './img/Sunny_socks_uni_red.jpg';
+            imagePath3 = './img/Sunny_socks_uni_yellow.jpg';
+            imagePath4 = './img/Sunny_socks_uni_blue.jpg';
+            imagePath5 = './img/Sunny_socks_uni_pink.jpg';
+            imagePath6 = './img/Sunny_socks_green.jpg';
+            imagePath7 = './img/Sunny_socks_red.jpg';
+            imagePath8 = './img/Sunny_socks_yellow.jpg';
+            imagePath9 = './img/Sunny_socks_blue.jpg';
+            imagePath10 = './img/Sunny_socks_pink_01.jpg';
+    }
+
+    // Update the image sources
+    imageElement1.src = imagePath1;
+    imageElement2.src = imagePath2;
+    imageElement3.src = imagePath3;
+    imageElement4.src = imagePath4;
+    imageElement5.src = imagePath5;
+    imageElement6.src = imagePath6;
+    imageElement7.src = imagePath7;
+    imageElement8.src = imagePath8;
+    imageElement9.src = imagePath9;
+    imageElement10.src = imagePath10;
+}
+
+
+            var imagePath1;
+            var imagePath2;
+            var imagePath3;
+            var imagePath4;
+            var imagePath5;
+            var imagePath6;
+            var imagePath7;
+            var imagePath8;
+            var imagePath9;
+            var imagePath10;
+
+            // Set the image paths based on the selected color
+            switch (phfil) {
+                case 'unicolor':
+                    imagePath1 = './img/Sunny_socks_uni_green.jpg';
+                    imagePath2 = './img/Sunny_socks_uni_red.jpg';
+                    imagePath3 = './img/Sunny_socks_uni_yellow.jpg';
+                    imagePath4 = './img/Sunny_socks_uni_blue.jpg';
+                    imagePath5 = './img/Sunny_socks_uni_pink.jpg';
+                    break;
+                case 'strip-socks':
+                    imagePath1 = './img/Sunny_socks_green.jpg';
+                    imagePath2 = './img/Sunny_socks_red.jpg';
+                    imagePath3 = './img/Sunny_socks_yellow.jpg';
+                    imagePath4 = './img/Sunny_socks_blue.jpg';
+                    imagePath5 = './img/Sunny_socks_pink_01.jpg';
+                    break;
+                case 'new-arrivals':
+                    imagePath1 = '../Webs/img/Sunny_socks_green.jpg';
+                    imagePath2 = '../Webs/img/packaging/catalogus_sokken_stripes_green.jpg';
+                    break;
+                // Colors
+                case 'green':
+                    imagePath1 = '../Webs/img/Sunny_socks_green.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_green.jpg';
+                    break;
+                case 'yellow':
+                    imagePath1 = '../Webs/img/Sunny_socks_yellow.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_yellow.jpg';
+                    break;
+                case 'red':
+                    imagePath1 = '../Webs/img/Sunny_socks_red.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_red.jpg';
+                    break;
+                case 'yellow':
+                    imagePath1 = '../Webs/img/Sunny_socks_yellow.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_yellow.jpg';
+                    break;
+                case 'blue':
+                    imagePath1 = '../Webs/img/Sunny_socks_blue.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_blue.jpg';
+                    break;
+                case 'pink':
+                    imagePath1 = '../Webs/img/Sunny_socks_pink_01.jpg';
+                    imagePath2 = '../Webs/img/Sunny_socks_uni_pink.jpg';
+                    break;
+                default:
+                    imagePath1 = './img/Sunny_socks_uni_green.jpg';
+                    imagePath2 = './img/Sunny_socks_uni_red.jpg';
+                    imagePath3 = './img/Sunny_socks_uni_yellow.jpg';
+                    imagePath4 = './img/Sunny_socks_uni_blue.jpg';
+                    imagePath5 = './img/Sunny_socks_uni_pink.jpg';
+                    imagePath6 = './img/Sunny_socks_green.jpg';
+                    imagePath7 = './img/Sunny_socks_red.jpg';
+                    imagePath8 = './img/Sunny_socks_yellow.jpg';
+                    imagePath9 = './img/Sunny_socks_blue.jpg';
+                    imagePath10 = './img/Sunny_socks_pink_01.jpg';
+            }
+            // Update the image sources
+            imageElement1.src = imagePath1;
+            imageElement2.src = imagePath2;
+            imageElement3.src = imagePath3;
+            imageElement4.src = imagePath4;
+            imageElement5.src = imagePath5;
+            imageElement6.src = imagePath6;
+            imageElement7.src = imagePath7;
+            imageElement8.src = imagePath8;
+            imageElement9.src = imagePath9;
+            imageElement10.src = imagePath10;
+        }
+        </script>
         <!-- end of the main section starting with footer now -->
         <footer>
             <?php include './view/layout/footer.php';?>
