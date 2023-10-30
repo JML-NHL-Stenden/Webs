a9708d3d986b896253d7bfc6035f8b8ae968668f
bochengpeng <146732012+bochengpeng@users.noreply.github.com>
2023-10-30 18:10:20 +0100

    Update checkout.php
    header out of body

--- checkout.php
+++ checkout.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="./view/layout/footer.css">
     <link rel="stylesheet" href="./css/checkout.css">
 </head>
+<?php include"./view/layout/header.php"?>
 <body>
-    <?php include"./view/layout/header.php" ?>
     <div class="billing wrapper">
         <h3 class="hd">Check out</h3>
         <h5>Billing details</h5>
