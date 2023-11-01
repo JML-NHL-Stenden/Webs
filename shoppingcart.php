c1b20f153080918012db7fe3da94de6669fd4981
FjodorSmorodins <fjodor.smorodins@student.nhlstenden.com>
2023-11-02 00:24:46 +0100

    shopping cart and selector in product details for single stripes

--- shoppingcart.php
+++ shoppingcart.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny socks - shopping cart</title>
-    <link rel="stylesheet" href="shoppingcart.css">
+    <link rel="stylesheet" href="./css/shoppingcart.css">
 </head>
 <body>
 
@@ -20,58 +20,100 @@ include "./view/layout/header.php"
 
 <!-- --------------------------------- MAIN ------------------------------- -->
 
-<main>
-        <div class="your-cart">
-            <h1>Your cart</h1>
-        </div>
-    <?php
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $count_of_pairs = filter_input(INPUT_POST, "number-input");
-        $color = filter_input(INPUT_POST, "color");
-        echo "
-        <div class='product box'>";
-        if ($color == "pink") {
-            echo "
-            <div class='single-product'>
-                <div class='socks-image'>
-                    <img src='./img/Sunny_socks_".$color."_01.jpg' alt=".$color."-socks>
-                </div>";
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $counts_of_pairs = filter_input(INPUT_POST, "counts_of_pairs", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+    $hasNonZeroValue = false;
+    $total_pair_count= 0;
+    foreach($counts_of_pairs as $color => $number) {
+        if (!empty($number)) {
+            $hasNonZeroValue = true;
         }
-        else {
-            echo "
-            <div class='single-product'>
-                <div class='socks-image'>
-                    <img src='./img/Sunny_socks_".$color.".jpg' alt=".$color."-socks>
-                </div>";
-        }
+    }
+
+    if ($hasNonZeroValue == true) {
         echo "
-                <div class='product-info'>
-                    <p><b>Color: </b>".$color."</p>
-                    <p><b>Pair count: </b>".$count_of_pairs."</p>
+        <main>
+            <div class='your-cart'>
+                <h1>Your cart</h1>
+            </div>
+            <div class='product-and-summary'>
+                <div class='product box'>";
+            foreach($counts_of_pairs as $color => $number) {
+                if (!empty($number)) {
+                        if ($color == "pink") {
+                        echo "
+                    <div class='single-product'>
+                        <div class='socks-image'>
+                            <img src='./img/Sunny_socks_".$color."_01.jpg' alt=".$color."-socks>
+                        </div>
+                        <div class='product-info'>
+                            <p><b>Color: </b>".$color."</p>
+                            <p><b>Pair count: </b>".$number."</p>
+                        </div>
+                    </div>";
+                        }
+                        else {
+                        echo "
+                    <div class='single-product'>
+                        <div class='socks-image'>
+                            <img src='./img/Sunny_socks_".$color.".jpg' alt=".$color."-socks>
+                        </div>
+                        <div class='product-info'>
+                            <p><b>Color: </b>".$color."</p>
+                            <p><b>Pair count: </b>".$number."</p>
+                        </div>
+                    </div>";
+                        }
+                        $total_pair_count += $number;
+                        $hasNonZeroValue = true;
+                }
+            }
+                echo "
+                </div>";
+                    $count_div_by_three_remain = $total_pair_count % 3;
+                    $count_rest = $total_pair_count - $count_div_by_three_remain;
+                    $price_for_remain = $count_div_by_three_remain * 4;
+                    $price_for_rest = $count_rest / 3 * 10;
+                    $total_price = $price_for_remain + $price_for_rest;
+                    echo "
+                <div class='order-summary box'>
+                    <h3>Order summary</h3>
+                    <div class='total-price'>
+                        <div class='total'><b>
+                            Total
+                        </b></div>
+                        <div class='price'>";
+                            echo $total_price." € 
+                        </div>
+                    </div>
+                    <div class='go-to-checkout'>
+                        <a href='Sunny_web_checkout/checkout.php'>Go to checkout <b>→</b></a>
+                    </div>
                 </div>
             </div>
-        </div>
-        "
-        ;
+        </main>";
     }
-    ?>
-        <div class="order-summary box">
-            <h3>Order summary</h3>
-            <div class="total-price">
-                <div class="total">
-                        Total
-                    </div>
-                    <div class="price">
-                        <?php
-                        echo 4 * $count_of_pairs." €";
-                        ?>
-                    </div>
+
+    else {
+        ?>
+        <main id="empty-cart">
+            <div class="img-container">
+                <img src="pictures/empty_cart.png" alt="empty-cart">
+            </div>
+            <h1><b>Your cart is empty and sad :&#40</b></h1>
+            <div class="button-area">
+                <div class="continue-shopping-button">
+                    <a href="Project1.php"><b>Continue shopping</b></a>
                 </div>
-            <a href="Sunny_web_checkout/checkout.php" class="go-to-checkout">
-                Go to checkout →
-            </a>
-        </div>
-</main>
+            </div>
+        </main>
+        <?php
+    }
+}
+?>
+    
+            
 
 <!-- --------------------------------- MAIN - END ------------------------------- -->
 
